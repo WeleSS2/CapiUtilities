bd0bed6951d2b2b9f23df690b1c4ca87ddd8ee82
Jakub Sadurski <jacubus@proton.me>
2025-04-29 18:32:16 +0200

    Docker init, no env ready

--- Docker/Python/start.py
+++ Docker/Python/start.py
@@ -1,43 +1,144 @@
+
 import os
 import sys
 import json
 
+argList = []
 venvList = []
-envName = ""
+nameVenv = ""
+nameExists = False
+
+debug = True
+debugLevel = 4
+
+def log(message, *args):
+    if debug:
+        print(message, *args)
+
+def generalLog(message, *args):
+    if debugLevel >= 4:
+        print(message, *args)
+
+def debugLog(message, *args):
+    if debugLevel >= 5:
+        print(message, *args)
+
+# ----------------------------------------------------------------------------
+
+def generateWebVenv():
+    
+    
+    return
+
+def generateDesktopVenv():
+    return
+
+def generateMobileVenv():
+    return
+
+def generateAuthVenv():
+    return
+
+def generateAdditionalPackages():
+    return
+
+# ----------------------------------------------------------------------------
+
+def transformArgs(args):
+    generalLog("Transforming args")
+
+    for arg in args:
+        debugLog("Argument: " + arg)
+        if "start.py" not in arg:
+            argList.append(arg)
+
+    args_to_concat = []
+    for arg in argList[:]:
+        debugLog("Current argument: " + arg)
+        if arg.startswith("--"):
+            debugLog("Encountered -- argument")
+            if args_to_concat:
+                debugLog("Concatenating accumulated args: " + str(args_to_concat))
+                concat_arg = " ".join(args_to_concat)
+                debugLog("Concatenated arg: " + concat_arg)
+                argList.insert(argList.index(arg), concat_arg)
+                debugLog("Inserted concatenated arg at position " + str(argList.index(concat_arg)))
+                args_to_concat = []
+        else:
+            debugLog("Accumulating non- -- argument: " + arg)
+
+            args_to_concat.append(arg)
+            argList.remove(arg)
+
+    # Final check for remaining args
+    if args_to_concat:
+        debugLog("Concatenating remaining args: " + str(args_to_concat))
+        concat_arg = " ".join(args_to_concat)
+
+        debugLog("Concatenated arg: " + concat_arg)
+        argList.append(concat_arg)
+
+
+    generalLog("Arguments: " + str(argList))
 
 def initWorkspace(args):
     if len(args) == 0:
-        print("No arguments provided")
+        generalLog("No arguments provided")
         return
     
-    for arg in args:
+    transformArgs(args)
+
+    for arg in argList:
+        if arg == "--name":
+            nameVenv = argList[argList.index(arg) + 1]
+            nameExists = True
+            generalLog("Name: %s", nameVenv)
+        else:
+            generalLog("Missing name, exiting")
+            exit(-1)
+
+    for arg in argList:
+        pos = argList.index(arg)
+        
         if arg == "--web":
-            venvList.append("web")
+            generateWebVenv()
+
         elif arg == "--desktop":
-            venvList.append("desktop")
+            generateDesktopVenv()
+        
         elif arg == "--mobile":
-            venvList.append("mobile")
-        elif arg == "--oauth":
-            venvList.append("oauth")
-        elif arg == "--name":
-            envName = args[args.index(arg) + 1]
+            generateMobileVenv()
+        
+        elif arg == "--auth":
+            generateAuthVenv()
+        
+        elif arg == "--all":
+            generateWebVenv()
+            generateDesktopVenv()
+            generateMobileVenv()
+            generateAuthVenv()
+
+        elif arg == "--add":
+            generateAdditionalPackages()
+            
 
     location = "/app-manager/pvenv"
 
-    venv_path = os.path.join(location, envName)
-    python_executable = sys.executable
-    command = f'{python_executable} -m {envName} {venv_path}'
+
+    
+    #venv_path = os.path.join(location, envName.replace(" ", "_"))
+    #python_executable = sys.executable
+    #command = f'{python_executable} -m {envName} {venv_path}'
 
     # Run the command to create the virtual environment
-    os.system(command)
+    #os.system(command)
     
-
     return
 
 def main(args):
-    print("Starting...")
+    generalLog("Starting...")
 
-    print("Arguments: ", args)
+    generalLog("Arguments: %s", args)
 
     # Load config from db
 
@@ -47,33 +148,26 @@ def main(args):
         config = json.load(f)
 
     if config is None:
-        print("No config found")
+        generalLog("No config found")
         return
     
     if config["initialized"] is False:
-        print("Creating workspace")
+        generalLog("Creating workspace")
         initWorkspace(args)
 
         return
         
-    for arg in args:
-        if arg == "--help":
-            print("Usage: start.py [options]")
-            print("Options:")
-            print("  --help  Show this help message")
-            return
-        
-
+main(sys.argv)
 
         
 
 # Get the location from the command-line argument
-location = "/app-manager/pvenv"
+#location = "/app-manager/pvenv"
 
 # Create the virtual environment
-venv_path = os.path.join(location, 'venv')
-python_executable = sys.executable
-command = f'{python_executable} -m venv {venv_path}'
+#venv_path = os.path.join(location, 'venv')
+#python_executable = sys.executable
+#command = f'{python_executable} -m venv {venv_path}'
 
 # Run the command to create the virtual environment
-os.system(command)
+#os.system(command)
