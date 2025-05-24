7ff1f9223283681a0e8e1a79fc433462532c5523
Pulsarnix <marek.mazurczak@protonmail.com>
2025-05-24 13:43:54 +0200

    Adjusted Dockefile, deleted scripts and config files

--- Projects/Python-desktop/docker.sh
+++
@@ -1,8 +0,0 @@
-docker container prune -f
-docker rmi -f python-test
-docker build -t python-test .
-docker run -it --rm \
-    --env DISPLAY=$DISPLAY \
-    --volume /tmp/.X11-unix:/tmp/.X11-unix \
-    --name python \
-    python-test
